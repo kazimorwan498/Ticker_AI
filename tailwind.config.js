b2130491e7d9189f951c65e200fa2feb88b51cef
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-11-03 22:17:03 +0600

    added header section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                sanMarino: "rgb(86, 119, 172)",
+                mariner: "rgb(66,102,158)",
+            },
+        },
+    },
+    plugins: [],
+};
